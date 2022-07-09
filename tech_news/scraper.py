76a43e8478af1409a0475f205d762a86b2422ede
Bruno Trindade <bruno@DESKTOP-I1C99NP.localdomain>
2022-07-09 20:44:29 -0300

    feat: scrape_next_page_link

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -30,7 +30,9 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    next_page = selector.css("span.current + a.page-numbers::attr(href)").get()
+    return next_page
 
 
 # Requisito 4
