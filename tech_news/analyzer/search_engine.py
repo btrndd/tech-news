fc02f891c1cd1b1b1873bce22792bf8e98177fff
Bruno Trindade <bruno@DESKTOP-I1C99NP.localdomain>
2022-07-10 19:48:50 -0300

    feat: search_by_title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,14 @@
+import re
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news_list = search_news({"title": re.compile(title, re.IGNORECASE)})
+    news_result = []
+    for news in news_list:
+        news_result.append((news["title"], news["url"]))
+    return news_result
 
 
 # Requisito 7
