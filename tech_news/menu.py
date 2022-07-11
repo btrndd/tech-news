13f97b131949e03b9d160030d894c3eeebaebe6a
Bruno Trindade <bruno@DESKTOP-I1C99NP.localdomain>
2022-07-10 21:59:13 -0300

    fix: string space error

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -12,16 +12,16 @@ def switch_options(option):
 
 def analyzer_menu():
     option = input("""
-    Selecione uma das opções a seguir:
-    0 - Popular o banco com notícias;
-    1 - Buscar notícias por título;
-    2 - Buscar notícias por data;
-    3 - Buscar notícias por tag;
-    4 - Buscar notícias por categoria;
-    5 - Listar top 5 notícias;
-    6 - Listar top 5 categorias;
-    7 - Sair.
-    """)
+Selecione uma das opções a seguir:
+0 - Popular o banco com notícias;
+1 - Buscar notícias por título;
+2 - Buscar notícias por data;
+3 - Buscar notícias por tag;
+4 - Buscar notícias por categoria;
+5 - Listar top 5 notícias;
+6 - Listar top 5 categorias;
+7 - Sair.
+""")
 
     if option == 0:
         input("Digite quantas notícias serão buscadas:")
